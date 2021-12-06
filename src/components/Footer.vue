1db0efaa4b2ef03821e346d40268282d24703d2b
david3214 <davidw3214@gmail.com>
2021-12-06 11:48:21 -0700

    initial Commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-footer
+    dark
+    padless
+  >
+    <v-card
+      flat
+      tile
+      color="secondary"
+      class="lighten-1 white--text text-center"
+      width='100%'
+    >
+      <v-card-text>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon[0]"
+          class="mx-4 white--text"
+          :href="icon[1]"
+          target="_blank"
+          icon
+        >
+          <v-icon size="24px">
+            {{ icon[0] }}
+          </v-icon>
+        </v-btn>
+      </v-card-text>
+
+      <v-card-text class="white--text pt-0">
+        Salt Lake City Area | David Westwood
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data: ()=> {
+    return {
+      icons: [
+        ['mdi-github', 'https://github.com/david3214'],
+        ['mdi-linkedin', 'https://www.linkedin.com/in/david-westwood-01354b225'],
+      ]
+    }
+  }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app-bar
+    app
+    color="primary"
+    dark
+    class="align-center"
+  >
+    <v-spacer></v-spacer>
+    <v-row class="align-center">
+      <v-btn plain dark 
+        v-for="directory in directories" 
+        :key="directory">
+        {{directory}}
+      </v-btn>
+    </v-row>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  data: ()=>{
+    return {
+      directories: [
+        "Resume",
+        "Portfolio",
+        "Contact"
+      ]
+    }
+  }
+}
+</script>
