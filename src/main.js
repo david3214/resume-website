8c10fe354f9ee803521b9a069115a5a881d25b80
david3214 <davidw3214@gmail.com>
2022-01-21 11:13:06 -0700

    Updated scroll bar to hover, and removed overscrolling on the page

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
+// import "overlayscrollbars/css/OverlayScrollbars.css";
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
+// import OverlayScrollbars from 'overlayscrollbars';
+// import { OverlayScrollbarsPlugin } from 'overlayscrollbars-vue';
 
 Vue.config.productionTip = false
 
@@ -10,3 +13,11 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+// Vue.use(OverlayScrollbarsPlugin)
+
+// OverlayScrollbars(document.body, {
+//   nativeScrollbarsOverlaid: {
+//       initialize: false
+//   }
+// });
