3d04a03619f9db57d57c1c7acb4094911c83aab0
david3214 <davidw3214@gmail.com>
2022-02-02 18:02:43 -0700

    Made rough draft of the contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,6 +4,14 @@
     src='../assets/office.jpeg'
     height='250px'/>
     <h1 class="titleText white--text">Contact</h1>
+    <v-row class="my-3" align='center'>
+        <v-col justify='start' align='center'>
+          <h2 >How to find me</h2>
+          <v-img class="picture" src="../assets/Sitting By Rocks.jpeg"/>
+          <h3>Email</h3>
+          <a href='mailto:davidw3214@gmail.com'>davidw3214@gmail.com</a>
+        </v-col>
+    </v-row>
   </v-main>
 </template>
 
@@ -15,5 +23,9 @@
   transform: translate(-50%, -50%);
   width: fit-content;
   font-size: clamp(3em, 7vw, 3.5em);
+}
+
+.picture {
+  width: 400px;
 }
 </style>
